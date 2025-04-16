baaeeb32496bb1221985532698db3694e50ecac6
Giovanni-Ortiz-Santos <gioortsan26@gmail.com>
2025-04-16 17:53:18 -0600

    Create grafico1.py

---
+++ Segundo semestre/Python/programas_no_organizados/grafico1.py
@@ -0,0 +1,6 @@
+from tkinter import*
+raiz=Tk() #la variable raiz toma el valor de modo grafico (ventana)
+raiz.title("TECNOLOGICO DE ESTUDIOS SUPERIORES DE JILOTEPEC")
+#RAIZ.reslizable(True,True) #se puede ampliar con mouse tanto horizontal como vertical
+raiz.geometry("700x400") #la ventana esta definida 700 horizontal por 400 vertical
+raiz.mainloop()
