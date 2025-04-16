25c8b1d51bfdad9fb0e1fa823a5658e71637f37c
Giovanni-Ortiz-Santos <gioortsan26@gmail.com>
2025-04-16 17:53:51 -0600

    Create Sentencia_if_Par_Impar.py

---
+++ Segundo semestre/Python/programas_no_organizados/Sentencia_if_Par_Impar.py
@@ -0,0 +1,9 @@
+# if simple
+
+print("*** FUNCION PAR E IMPAR ***")
+
+n_valor = int(input("Ingresa un valor: "))
+if n_valor % 2 == 0:
+    print(f"El numero {n_valor} es par")
+else:
+    print(f"El numero {n_valor} es impar")
