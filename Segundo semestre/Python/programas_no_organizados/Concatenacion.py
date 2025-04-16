99ae0d752439cc73d6cc493f5c37efbc63c007e4
Giovanni-Ortiz-Santos <gioortsan26@gmail.com>
2025-04-16 17:53:09 -0600

    Create Concatenacion.py

---
+++ Segundo semestre/Python/programas_no_organizados/Concatenacion.py
@@ -0,0 +1,6 @@
+x1 = input("Dame un numero")
+x2 = input("Dame otro numero")
+x3 = input("Dame un tercer numero")
+
+concatena = str(x1)+ str(x2)+str(x3)
+print("El resultado de la concatenacion es: ", concatena)
